4ac02501e354b3a9893f955973ca455e2c5404aa
Anthony Ranieri <anthonyjaranieri@gmail.com>
2023-02-01 10:54:17 -0500

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -38,40 +38,87 @@ const cardTemplate = document
   .querySelector(".template")
   .content.querySelector(".card");
 
+const profileAddButton = document.querySelector(".profile__add-button");
+const formButtonReset = document.querySelector(".form__button-reset");
+const form = document.querySelector(".form");
+const profileFormStructure = document.querySelector(".form__structure");
+const titleInput = document.querySelector("#title");
+const imageInput = document.querySelector("#image");
+const cardText = document.querySelector(".card__text");
+
 function openProfileModal() {
   modal.classList.add("modal_opened");
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
-
 function closeModal() {
   modal.classList.remove("modal_opened");
 }
-
 profileEditButton.addEventListener("click", openProfileModal);
 modalButtonReset.addEventListener("click", closeModal);
 
-function handleProfileFormSubmit(evt) {
+function handleProfileModalSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
   closeModal();
 }
+modal.addEventListener("submit", handleProfileModalSubmit);
 
-modal.addEventListener("submit", handleProfileFormSubmit);
+function openProfileForm() {
+  form.classList.add("form_opened");
+}
+function closeForm() {
+  form.classList.remove("form_opened");
+}
+profileAddButton.addEventListener("click", openProfileForm);
+formButtonReset.addEventListener("click", closeForm);
+
+function handleCardFormSubmit(evt) {
+  evt.preventDefault();
+  cardsList.prepend(
+    getCardElement({ link: imageInput.value, name: titleInput.value })
+  );
+  closeForm();
+}
+form.addEventListener("submit", handleCardFormSubmit);
 
 function getCardElement(data) {
   const cardElement = cardTemplate.cloneNode(true);
   const cardText = cardElement.querySelector(".card__text");
   const cardImage = cardElement.querySelector(".card__image");
+  const cardLikeButton = cardElement.querySelector(".card__like-button");
+  const cardTrashButton = cardElement.querySelector(".card__trash-button");
+
+  const picture = document.querySelector(".picture");
+  const pictureButtonReset = document.querySelector(".picture__button-reset");
+  const pictureImageClicked = document.querySelector(".picture__image_closed");
+  const pictureImageText = document.querySelector(".picture__image_text");
 
   cardImage.src = data.link;
   cardImage.alt = data.name;
   cardText.textContent = data.name;
+  cardLikeButton.addEventListener("click", function () {
+    cardLikeButton.classList.toggle("card__like-button_active");
+  });
+
+  cardTrashButton.addEventListener("click", function () {
+    cardElement.remove();
+  });
+
+  cardImage.addEventListener("click", function () {
+    picture.classList.add("picture_opened");
+    pictureImageClicked.src = data.link;
+    pictureImageText.textContent = data.name;
+  });
+  pictureButtonReset.addEventListener("click", function () {
+    picture.classList.remove("picture_opened");
+  });
+
   return cardElement;
 }
-const cardsList = document.querySelector(".cards__list");
 
-for (let i = 0; i < initialCards.length; i++) {
-  cardsList.prepend(getCardElement(initialCards[i]));
-}
+const cardsList = document.querySelector(".cards__list");
+initialCards.forEach(function (item) {
+  cardsList.prepend(getCardElement(item));
+});
